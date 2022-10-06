2314ba4cb1d706590c6b31a7e75a2ec2bf015d52
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-10-05 21:12:44 -0300

    feat: salvei as tarefas no banco de dados, falta corrigir uns detalhes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,108 +1,157 @@
 import React from 'react';
-import { useState} from 'react';
+
+import { useState } from 'react';
 import { CustomSection } from '../components/StylesComponents';
-import  {useNavigate, Link} from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios';
 
 //component
+
 import image from '../img/tasksEmpty.png';
 import { MessageInput } from '../components/StylesComponents';
 import Task from './Task';
-import Modal  from '../components/Modal';
+import Modal from '../components/Modal';
 import * as C from '../components/StylesComponents';
 import TasksCompleted from './TasksCompleted';
+
 //styles
 
 import styled from 'styled-components'
-import {GrAdd}from 'react-icons/gr'
-import {AiOutlineDelete,AiFillEdit} from 'react-icons/ai'
+import { GrAdd } from 'react-icons/gr'
+import { AiOutlineDelete, AiFillEdit } from 'react-icons/ai'
 
 
 // colors
 
 import colors from '../colors/colors.style.jsx';
 import { useEffect } from 'react';
- 
+
 
 
 
 function List() {
 
-  const[task, setTask]= useState('')
-  const[tasks, setTasks] = useState([])
-  const[messageInput, setMessageInput] = useState('')
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [messageInput, setMessageInput] = useState('')
 
 
-  const[edit,setEdit] = useState('')
-  const[id, setId] = useState(0)
-  const[modal,setModal] = useState(false)
-  const[valueEdit, setValueEdit]= useState()
+  const [edit, setEdit] = useState('')
+  const [id, setId] = useState(0)
+  const [modal, setModal] = useState(false)
+  const [valueEdit, setValueEdit] = useState()
 
-  
-  const[newId,setNewId]= useState()
-  const[newTime, setNewTime]= useState()
-  
+
+  const [newId, setNewId] = useState()
+  const [newTime, setNewTime] = useState()
+
   const totalCompleted = [];
   let qtdCompleted = []
-   
-  
 
 
+  const saveTasks = async(props)=>{
 
-
-  function handleTasks(){
+    await axios.post("https://fsc-task-manager-backend.herokuapp.com/tasks",{
+    description: task,
+    isCompleted:false,
     
-    setId(id+1)
+  })
+}
+
+
+  const fetchTasks = async () => {
+
+    try {
+
+      const response = await axios.get("https://fsc-task-manager-backend.herokuapp.com/tasks")
+       setTasks([...response.data])
   
+      
+    
+    }
+
+    catch(error) {
+
+    }
+
+  }
+
+  useEffect(() => {
+
+    fetchTasks()
+
+  }, [])
+
+
+
+const handleTasks = async ()=> {
+      
+   
+ 
+
+    setId(id + 1)
+
     const newTask = {
       task: task,
-      id:id,
+      id: id,
       time: new Date().toLocaleString('pt-br', {
-        hour:'2-digit',
-        minute:'2-digit',
-        second:'2-digit',
-    
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+
       })
 
     }
-      
-     if(task.length === 0){
+     
+
+
+    if (task.length === 0) {
       setMessageInput('write something!')
       return
-     }
-
-     //remove 
-
-     setTasks(prevTasks =>[...prevTasks, newTask])
-     setMessageInput('')
-     setTask('')
+    }
     
-  }
- 
-  const HandleRemove = (id) =>{
-    setTasks(tasks.filter(tasks => tasks.id != id))
-   }
+    
+    setTasks(prevTasks => [...prevTasks, newTask])
+    setMessageInput('')
+    setTask('')
 
- 
+    try{
+      
+      await saveTasks()
+    }
+    catch(error){
+
+    }
+
+  }
+
+  //remove 
+
+  const HandleRemove = (id) => {
+    setTasks(tasks.filter(tasks => tasks.id != id))
+  }
+
+
   //modal
 
-  const handleModal = (tasksvalue,id) =>{
-     setModal(!modal);
-     setValueEdit(tasksvalue)
-     setNewId(id)
-     
+  const handleModal = (tasksvalue, id) => {
+    setModal(!modal);
+    setValueEdit(tasksvalue)
+    setNewId(id)
+
   }
-   
+
   //edit
 
-  const handleEdit = (newValue,currentId,NewTime) => {
-    
+  const handleEdit = (newValue, currentId, NewTime) => {
+
 
     const totalTasks = [...tasks]
-    totalTasks.splice(currentId,1,{task:newValue,id:currentId,time:NewTime})
+    totalTasks.splice(currentId, 1, { task: newValue, id: currentId, time: NewTime })
     setTasks(totalTasks)
 
-  
-  
+
+
   }
 
 
@@ -110,102 +159,104 @@ function List() {
   //completed
 
 
-  
 
 
-  const isCompleted = (completed) =>{
-    
-     //   !!PENDENCIAS!!
 
-     //refazer a lista após completar as tasks, copiando as tasks completas 
-     //e as enviando como props e deletar da currenty
-    
-    const  complete = tasks.filter(tasks => tasks.id === completed.id )
- 
-     for(let i of complete){
-         totalCompleted.push([i])
-     }
+  const isCompleted = (completed) => {
 
-    
-     qtdCompleted = [...totalCompleted]
-     const qtd = document.querySelector('.qtd')
-     qtd.classList.add('active')
-     qtd.innerText = qtdCompleted.length;
-     
-      
-   }
+    //   !!PENDENCIAS!!
+
+    //refazer a lista após completar as tasks, copiando as tasks completas 
+    //e as enviando como props e deletar da currenty
+
+    const complete = tasks.filter(tasks => tasks.id === completed.id)
+
+    for (let i of complete) {
+      totalCompleted.push([i])
+    }
 
 
- const navigate = useNavigate()
+    qtdCompleted = [...totalCompleted]
+    const qtd = document.querySelector('.qtd')
+    qtd.classList.add('active')
+    qtd.innerText = qtdCompleted.length;
 
- const handleNavigate = () => {
 
-   
-   navigate('/TasksCompleted',{
-    state:{
-           completed:[...totalCompleted] }
-      })
- }
+  }
 
- // instalar a lib react-toastfy pra adicionar pop ups com menssagens
 
-  return(
-  
-   
-    <CustomSection qtdCompleted={qtdCompleted}>
-    <h2>Task - Today</h2>
-     
+  const navigate = useNavigate()
 
-       <input type="search" 
-              onChange={(e)=>setTask(e.target.value)}
-              value={task}
-              placeholder =" new task..." className='inptAdd' />
+  const handleNavigate = () => {
 
-       <button onClick={handleTasks} className='add'>
-          add
-         <GrAdd className='spanButton'/>
-       </button>
-       
-       <MessageInput >{messageInput}</MessageInput>
-       <p className='currenty'>Currenty tasks: {tasks.length}</p>
-    
-        <button onClick={()=> handleNavigate()} className='completed'>Completed Tasks 
-        <span className='qtd'></span>
-        </button>
-       
-    
 
-  
-     { modal && <Modal open={handleModal} tasks={valueEdit} id={newId} handleEdit ={handleEdit} time={newTime}/>}
-       
-       {
-       
-       tasks.length === 0? 
-          <img src={image} alt="image tasks empty" />:
-       <C.Container>
-          {
-          tasks.map(tasks => <Task
-                             
-                             isCompleted={isCompleted}
-                             name={tasks.task}
-                             key={tasks.id} 
-                             time={tasks.time} 
-                             id={tasks.id}
-                             remove={() => HandleRemove(tasks.id)}
-                             modal={()=> handleModal(tasks.task,tasks.id,setNewTime(tasks.time))}
-                              />
-                        )
+    navigate('/TasksCompleted', {
+      state: {
+        completed: [...totalCompleted]
       }
+    })
+  }
 
-       </C.Container>}
-   
-  </CustomSection>
-  
+  // instalar a lib react-toastfy pra adicionar pop ups com menssagens
+
+  return (
 
 
-  )}  
+    <CustomSection qtdCompleted={qtdCompleted}>
+      <h2>Task - Today</h2>
 
-  
-  
-  
-  export default List
+
+      <input type="search"
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+        placeholder=" new task..." className='inptAdd' />
+
+      <button onClick={handleTasks} className='add'>
+        add
+        <GrAdd className='spanButton' />
+      </button>
+
+      <MessageInput >{messageInput}</MessageInput>
+      <p className='currenty'>Currenty tasks: {tasks.length}</p>
+
+      <button onClick={() => handleNavigate()} className='completed'>Completed Tasks
+        <span className='qtd'></span>
+      </button>
+
+
+
+
+      {modal && <Modal open={handleModal} tasks={valueEdit} id={newId} handleEdit={handleEdit} time={newTime} />}
+
+      {
+
+        tasks.length === 0 ?
+          <img src={image} alt="image tasks empty" /> :
+          <C.Container>
+            {
+              tasks.map(tasks => <Task
+
+                isCompleted={isCompleted}
+                name={tasks.task}
+                key={tasks.id}
+                time={tasks.time}
+                id={tasks.id}
+                remove={() => HandleRemove(tasks.id)}
+                modal={() => handleModal(tasks.task, tasks.id, setNewTime(tasks.time))}
+              />
+              )
+            }
+
+          </C.Container>}
+
+    </CustomSection>
+
+
+
+  )
+}
+
+
+
+
+export default List
